61c3a7c61bad4f7594c12f57d90bc0ffa8c172c2
Albert Hsieh <aehsieh@gmail.com>
2020-11-24 10:28:45 -0800

    notification

--- js/main.js
+++ js/main.js
@@ -5,5 +5,18 @@ window.onload = () => {
     console.log("registered worker");
     navigator.serviceWorker
              .register('./sw.js');
+    displayNotification();
+  }
+}
+
+Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+});
+
+function displayNotification() {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      reg.showNotification('Hello world!');
+    });
   }
 }
